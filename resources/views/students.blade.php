3f6a90e2a60f6c79f02c199dd7a9658f9f9a9383
= <alitunacanonar@icloud.com>
2021-06-16 12:45:45 +0300

    User additions have been made.

---
+++ resources/views/students.blade.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"></script>
+    <title>Laravel Ajax</title>
+</head>
+<body>
+    
+    <section style="padding-top:60px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            Students 
+                            <a href="" class="btn btn-sm btn-secondary float-right"  data-toggle="modal" data-target="#studentModal">Add Student</a>
+                        </div>
+                        <div class="card-body">
+                            <table id="studentTable" class="table">
+                                <thead>
+                                    <tr>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Email</th>
+                                        <th>Phone</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($students as $student)
+                                    <tr>
+                                        <td>{{ $student->firstname }}</td>
+                                        <td>{{ $student->lastname }}</td>
+                                        <td>{{ $student->email }}</td>
+                                        <td>{{ $student->phone }}</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Modal -->
+    <div class="modal fade" id="studentModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content"> 
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add New Student</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form id="studentForm">
+                        @csrf
+                        <div class="form-group">
+                            <label for="firstname">First Name</label>
+                            <input type="text" class="form-control" id="firstname" />
+                        </div>
+                        <div class="form-group">
+                            <label for="lastname">Last Name</label>
+                            <input type="text" class="form-control" id="lastname" />
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" class="form-control" id="email" />
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Phone</label>
+                            <input type="text" class="form-control" id="phone" />
+                        </div>
+                        
+                        <button type="submit" class="btn btn-sm btn-primary float-right">Submit</button>
+                        
+                    </form>
+                </div>
+            
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $("#studentForm").submit(function(e){
+            e.preventDefault();
+
+            let firstname = $("#firstname").val();
+            let lastname = $("#lastname").val();
+            let email = $("#email").val();
+            let phone = $("#phone").val();
+            let _token = $("input[name=_token]").val();
+
+            $.ajax({
+                url:"{{ route('student.add' )}}",
+                type:"POST",
+                data:{
+                    firstname:firstname,
+                    lastname:lastname,
+                    email:email,
+                    phone:phone,
+                    _token:_token
+                },
+                success:function(response)
+                {
+                    if(response){
+                        $("#studentTable tbody").prepend('<tr><td>'+ response.firstname +'</td><td>'+ response.lastname +'</td><td>'+ response.email +'</td><td>'+ response.phone +'</td></tr>');
+                        $("#studentForm")[0].reset();
+                        $("#studentModal").modal('hide');
+                    }
+                }
+            }); 
+        });
+    </script>
+    
+</body>
+</html>
